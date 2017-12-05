41b3819479d1097cdfca962e6155403a39be793e
Pierre-Alexis Bayen <bayen.pierre@gmail.com>
2017-12-05 11:29:41 +0100

    facebook setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
 
-  get 'users/show'
+  # get 'users/show'
 
-  get 'users/edit'
+  # get 'users/edit'
 
-  get 'users/update'
+  # get 'users/update'
 
-  get 'users/destroy'
+  # get 'users/destroy'
 
-  devise_for :registrations, :controllers => { registrations: "registrations"}
+  devise_for :registrations, :controllers => {
+    registrations: "registrations",
+    omniauth_callbacks: 'registrations/omniauth_callbacks'
+}
+
   root to: 'pages#home'
+
+  # devise_for :registrations,
+    # controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
