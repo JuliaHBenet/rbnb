dace04c591bcf14f9551cd6811ca32b495bb705b
Julia Hernandez Benet <juliahbenet@gmail.com>
2017-12-05 10:15:34 +0100

    start facebook login

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,7 @@ class Registration < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   has_one :user
 end
