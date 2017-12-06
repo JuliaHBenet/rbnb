7ddbede892e56d66bee7fc0462c37ee956ad03d8
Madiha Latafi <latafimadiha@hotmail.fr>
2017-12-06 16:28:50 +0100

    Booking debugged

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,10 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :status, presence: true
 
   def total_price
-    self.total_price = booking.pet.daily_price * (((end_date - start_date).to_i) - 1)
+    self.total_price = self.pet.daily_price * (((end_date - start_date).to_i) - 1)
     self.save
   end
 end
